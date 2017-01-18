7b621cf523964513b39afc0248d324e800b3bbc4
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2017-01-18 14:12:49 +0900

    2017/01/18

--- src/main/java/springmvc01/control/ex7/MyBindingInitializer.java
+++ src/main/java/springmvc01/control/ex7/MyBindingInitializer.java
@@ -18,3 +18,26 @@ public class MyBindingInitializer implements WebBindingInitializer{
     binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
   }
 }
+
+/* 이 객체를 스프링 설정 파일에 등록해야 한다.
+=> Spring 2.5 방식
+<bean class="org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter">
+    <property name="cacheSeconds" value="0"/>
+    <property name="webBindingInitializer">
+        <bean class="springmvc01.control.ex7.MyBindingInitializer"/>
+    </property>
+</bean> 
+=> Spring 3.2 방식
+1) MVC 관련 애노테이션을 다룰 객체를 등록해야 한다.
+<mvc:annotation-driven/>  
+2) 전역에서 사용할 수 있도록 커스텀 에디터를 등록한다.
+<bean class="org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter">
+    <property name="order" value="0" /> <== 주의! 적용 순서를 최우선으로 해야 한다.
+    <property name="cacheSeconds" value="0"/>
+    <property name="webBindingInitializer">
+        <bean class="springmvc01.control.ex7.MyBindingInitializer"/>
+    </property>
+</bean> 
+=> Spring 4.0 방식
+@ControllerAdvice 애노테이션을 사용하여 설정한다.
+ */
